c4dadfb559d9fbb8cb3849062889ba9f6348a019
Kayahs <akshaykmanchanda@gmail.com>
2019-01-08 09:21:22 -0500

    Reworked framework for RAW version of site.

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 <html>
 <head>
     <title></title>
-    <link rel="stylesheet" href="styles/style.css" />
+    <link rel="stylesheet" href="src/css/style.css" />
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!-- <script src="src/js/scripts.js"></script> -->
 </head>
 <body>
-  
+  <div class="splash-div">
+    <div class="code-switch-icon">
+    </div>
+    <div class="splash-name">
+      Akshay Manchanda
+    </div>
+    <div class="splash-title">
+      Full Stack Developer
+    </div>
+    <div class="scroll-arrow">
+    </div>
+  </div>
 </body>
 </html>
